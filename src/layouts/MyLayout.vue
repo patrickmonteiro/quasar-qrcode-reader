78d47333b9342c70440560d66200b55aee7d9e6b
patryckx <patrick.monteiro@w3as.com>
2019-07-17 11:52:40 -0300

    First commit :tada:

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-black">
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Docs Quasar</q-item-label>
+            <q-item-label caption>quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Github Quasar</q-item-label>
+            <q-item-label caption>github.com/quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://github.com/patrickmonteiro/quasar-qrcode-reader">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Github Project</q-item-label>
+            <q-item-label caption>github.com/patrickmonteiro/quasar-qrcode-reader</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page class="row full-height items-center">
+    <div class="col text-center">
+      <q-btn color="black" icon="camera_alt" label="Read QRCode"
+        class="full-width" size="lg" @click="turnCameraOn()"
+        v-show="!showCamera"/>
+
+        <p class="text-subtitle1" v-if="result">Last result: <b>{{ result }}</b></p>
+        <div v-if="showCamera">
+
+          <qrcode-stream :camera="camera" @decode="onDecode">
+          </qrcode-stream>
+        </div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import { QrcodeStream } from 'vue-qrcode-reader'
+export default {
+  name: 'PageIndex',
+  components: { QrcodeStream },
+  data () {
+    return {
+      isValid: undefined,
+      camera: 'auto',
+      result: null,
+      showCamera: false
+    }
+  },
+  methods: {
+    async onDecode (content) {
+      this.result = content
+      this.turnCameraOff()
+    },
+
+    turnCameraOn () {
+      this.camera = 'auto'
+      this.showCamera = true
+    },
+
+    turnCameraOff () {
+      this.camera = 'off'
+      this.showCamera = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.validation-success,
+.validation-failure,
+.validation-pending {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, .8);
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.4rem;
+  padding: 10px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
+.validation-success {
+  color: green;
+}
+.validation-failure {
+  color: red;
+}
+</style>
